c73e25aa9161402c3639f437925df332a26d9726
aasth <aasthajha123@gmail.com>
2024-02-19 22:17:33 -0800

    Fixed /register and /login

--- app.py
+++ app.py
@@ -3,34 +3,41 @@ import uuid
 from flask import Flask, request, jsonify, make_response
 import hashlib
 import logging
-# from flask_talisman import Talisman
-
-app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 #initalize logger
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
-#initlize talisman with default settings
-# talisman = Talisman(app)
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(minutes=15)
+db = SQLAlchemy(app)
 
-#initializa db and create connection
-conn = sqlite3.connect("app.db",timeout=20)
-cursor = conn.cursor()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    session_token = db.Column(db.String(50))
 
-#create users table
-cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY,username TEXT UNIQUE,password TEXT, role TEXT)")
 
-#create admin user role if it doesn't exist
-result = cursor.execute("SELECT COUNT(*) FROM users WHERE role='admin' ")
-if result==0:
-    #encrypt the password 'admin'
-    admin_password = hashlib.sha256('admin'.encode()).hexdigest()
-    cursor.execute("INSERT INTO users (username,password,role) VALUES (?,?,?)",('admin',admin_password,'admin'))
+def admin_user():
+    with app.app_context():
+        db.create_all()
+        # Check if admin user exists
+        admin_user = User.query.filter_by(username='admin').first()
+        if not admin_user:
+            # Create admin user
+            admin_password = hashlib.sha256('admin'.encode()).hexdigest()
+            admin_user = User(username='admin', password=admin_password, role='admin')
+            db.session.add(admin_user)
+            db.session.commit()
+            app.logger.info("Created Admin user")
 
-#close connection
-conn.commit()
-conn.close()
 
 @app.route('/register',methods=['POST'])
 def register_user():
@@ -39,20 +46,18 @@ def register_user():
 
     if not username or not password:
         return make_response(jsonify({'message':"Both username and password must be provided"}),400)
-    
-    with sqlite3.connect("app.db",timeout=20) as conn:
-        cursor = conn.cursor()
 
-        result = cursor.execute("SELECT COUNT(*) FROM users WHERE username= (?)",(username,))
-        result = cursor.fetchone()[0] 
-        if result>0:
-            return make_response(jsonify({'message':"Username already exists!"}),400)
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return make_response(jsonify({'message':"Username already exists!"}),400)
 
-        password = hashlib.sha256(password.encode()).hexdigest()
-        cursor.execute("INSERT INTO users (username,password,role) VALUES (?,?,?)",(username,password,'user'))
-        conn.commit()
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    new_user = User(username=username, password=hashed_password, role='user')
+    db.session.add(new_user)
+    db.session.commit()
 
     return make_response('',201)
+
 
 @app.route('/login',methods=['POST'])
 def login_user():
@@ -61,26 +66,20 @@ def login_user():
 
     if not username or not password:
         return make_response(jsonify({'message':"Both username and password must be provided"}),400)
-    
-    conn = sqlite3.connect("app.db")
-    cursor = conn.cursor()
 
-    password = hashlib.sha256(password.encode()).hexdigest()
-    cursor.execute("SELECT * FROM users WHERE username=? and password=?",(username,password))
-    result = cursor.fetchone()
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    user = User.query.filter_by(username=username, password=hashed_password).first()
 
-    if result:
+    if user:
         session_token = str(uuid.uuid4())
-        cursor.execute("UPDATE users SET session_token=? WHERE id=?",(session_token,result[0]))
-        conn.commit()
-        conn.close()
+        user.session_token = session_token
+        db.session.commit()
         response = make_response('',201)
         response.set_cookie('session_token',session_token)
         return response
     else:
-        conn.commit()
-        conn.close()
         return make_response(jsonify({'message':'Invalid credentials. Check username and password again.'}),401)
 
 if __name__ == '__main__':
+    admin_user()
     app.run(debug=True)
--- readMe.md
+++ readMe.md
@@ -12,11 +12,17 @@ Run `python app.py` in a command line with the venv activated.
 
 In another command line, run `curl -X POST http://localhost:5000/register -H "Content-Type: application/json" -d "{\"username\": \"aastha\", \"password\": \"user\"}"` to test any endpoint (in this case I am testing the `/register` endpoint)
 
+You can see the response of the `curl` command in the `app.log` file or in the command line itself (depending on the request).
+
+### Example of app.log:
+
+![alt text](/images/app_log.png)
+
 # To do:
 
-1. Check if all endpoints are working correctly as I haven't tested them yet
-2. Secure the code by adding logging. I also saw something called Talisman but not sure if it helps
-3. Clean up code - Add .gitignore /comments/ formatting
-4. Need to set up docker to containerize it (?) 
+1. Check if all endpoints are working correctly as I haven't tested them yet (tested register and login) ✅
+2. Add remaining 3 endpoints and test it
+3. Secure the code by adding logging. I also saw something called Talisman but not sure if it helps
+4. Clean up code - Add .gitignore /comments/ formatting
+5. Need to set up docker to containerize it (?) 
 
-If I test an endpoint continuously, I get `sqlite3.OperationalError: database is locked ` error. Need to have a workaround for this.
